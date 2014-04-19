3f92de155a320d00f02874bec172d3c6855c89da
Adam Georgeson <adam.georgeson@sage.com>
2014-04-19 10:27:27 +0100

    two releases in a day working out wrong version number

--- app/models/release.rb
+++ app/models/release.rb
@@ -4,7 +4,12 @@ class Release < ActiveRecord::Base
   validates_presence_of :date
   paginates_per 10
 
-  def self.version app, date
-    self.where("#{app} != '' AND date <= ?", date).pluck(app).sort_by(&:to_i).last || "-"
+  def self.version app, release
+    if release[app].present?
+      release[app]
+    else
+      @date = release.date
+      self.where("#{app} != '' AND date <= ?", @date).pluck(app).sort_by(&:to_i).last || "-"
+    end
   end
 end
