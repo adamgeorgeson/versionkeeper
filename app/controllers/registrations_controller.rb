604550488389a14fd1d9d388f5c4f8c8858cff30
Adam Georgeson <adam.georgeson@sage.com>
2014-05-15 22:31:28 +0100

    x

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
-  def after_inactive_sign_up_path_for
+  def after_inactive_sign_up_path_for(resource)
     '/users/sign_in'
   end
 end
