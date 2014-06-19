5bcb957af5b20ac405aa82f3e3fea9d29dd51e9a
Adam Georgeson <adam.georgeson@sage.com>
2014-06-19 22:00:43 +0100

    Markdown autolinks and doesn't parse emphasis inside words i.e. i_like_underscores

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,7 +15,7 @@ module ApplicationHelper
     }
  
     renderer = Redcarpet::Render::HTML.new(options)
-    markdown = Redcarpet::Markdown.new(renderer, extensions)
+    markdown = Redcarpet::Markdown.new(renderer, extensions = {no_intra_emphasis: true, autolink: true})
  
     markdown.render(text).html_safe
   end
